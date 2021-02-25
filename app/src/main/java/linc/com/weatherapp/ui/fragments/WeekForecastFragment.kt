f58a89993a329545009000fd6248edabd5d9ee69
Andrew <andriy.serb1@gmail.com>
2021-02-25 20:39:23 +0100

    Week forecast fragment without markup

---
+++ app/src/main/java/linc/com/weatherapp/ui/fragments/WeekForecastFragment.kt
@@ -0,0 +1,16 @@
+package linc.com.weatherapp.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import linc.com.weatherapp.R
+
+class WeekForecastFragment : Fragment(R.layout.fragment_week_forecast) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
