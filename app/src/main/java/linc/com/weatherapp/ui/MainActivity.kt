64c2564d7a26492710ef13e2e06e37f6502ad33c
Andrew <andriy.serb1@gmail.com>
2021-07-14 17:51:52 +0300

    Background video

--- app/src/main/java/linc/com/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/linc/com/weatherapp/ui/MainActivity.kt
@@ -1,12 +1,28 @@
 package linc.com.weatherapp.ui
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
 import linc.com.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/linc/com/weatherapp/ui/main/MainForecastFragment.kt
+++ app/src/main/java/linc/com/weatherapp/ui/main/MainForecastFragment.kt
@@ -1,21 +1,24 @@
 package linc.com.weatherapp.ui.main
 
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.drawToBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.load.resource.bitmap.BitmapDrawableDecoder
-import com.github.matteobattilana.weather.PrecipType
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import linc.com.weatherapp.R
 import linc.com.weatherapp.databinding.FragmentMainForecastBinding
 import linc.com.weatherapp.ui.main.items.DailyItem
 import linc.com.weatherapp.utils.TimestampFormatter
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class MainForecastFragment : Fragment() {
 
@@ -46,6 +49,15 @@ class MainForecastFragment : Fragment() {
             mainForecastViewModel.dailyItemsState.collect {
                 dailyGroupAdapter.addAll(it.map { daily -> DailyItem(daily) })
             }
+        }
+
+        // TODO: 14.07.21 Restart bug => onResume
+        mainForecastBinding.background.apply {
+            setOnPreparedListener {
+                it.isLooping = true
+            }
+            setVideoRaw(R.raw.forest_result)
+            start()
         }
 
     }
